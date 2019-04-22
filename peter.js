b04cd54495966b8a1b797433682bfab00d7ea401
Peter Black <1300507@abertay.ac.uk>
2019-04-22 17:40:46 +0200

    Prettified the background and made it a bit faster

--- peter.js
+++ peter.js
@@ -4,22 +4,41 @@ function mrblackplay()
     audio.play();
 }
 
-// update on any window size change.
-window.addEventListener("resize", Resize);
-setInterval(Update, 30);
-window.onload = Resize;
-
+///////////////////////////////////////////////////////////////
+//////////////////////////WINDOW HANDLES
+///////////////////////////////////////////////////////////////
+var mouseDown = 0;
+var lastMousePos = [];
 var created = false;
 
+window.addEventListener("resize", Resize);
+window.addEventListener('mousedown', Update, false);
+window.addEventListener('mousemove', setMousePos, false);
+document.body.onmousedown = function(evt) { if(evt.button == 0)mouseDown = 1; }
+document.body.onmouseup = function(evt) { if(evt.button == 0)mouseDown = 0; }
+window.onload = Resize;
+setInterval(Update, 20);
+
+
+///////////////////////////////////////////////////////////////
+//////////////////////////GLOBAL
+///////////////////////////////////////////////////////////////
 var niceColour = 'pink';
 var pointsArray = [];
 var pointsVelocities = [];
-var numPoints = 50;
-var distanceForLine = 175;
-var speed = 0.2;
-var maxVelocity = 1;
 
-function Update()
+var resizeRespawnThreshold = 200;
+
+var maxNumPoints = 70;
+var numPoints = 0;
+var distanceForLine = 250;
+
+var speed = 0.05;
+var maxVelocity = 0.5;
+var clickMoveInvPower = 5;
+
+
+function Update(e)
 {
     ///////////////////////////////////////////////////////////////
     //////////////////////////GLOBAL
@@ -27,24 +46,39 @@ function Update()
     var canvas = document.getElementById('background');
     var ctx = canvas.getContext('2d'); 
     var i = 0;//loop
+    
     ///////////////////////////////////////////////////////////////
     //////////////////////////CREATION
     ///////////////////////////////////////////////////////////////
     if(created == false)
     {
-        for(i = 0; i < numPoints; i++)
+        var numPointsToCreate = maxNumPoints;
+        if(canvas.width < canvas.height)
+        {
+            numPointsToCreate = maxNumPoints / 2;
+        }
+        
+        for(i = 0; i < numPointsToCreate; i++)
         {
             var randomX = Math.floor(Math.random() * canvas.width);
             var randomY = Math.floor(Math.random() * canvas.height);
             pointsArray[i] = [randomX, randomY];
             pointsVelocities[i] = [0,0];
         }
+        numPoints = numPointsToCreate;
         created = true;
     }
     
     ///////////////////////////////////////////////////////////////
     //////////////////////////UPDATE
     ///////////////////////////////////////////////////////////////
+    var pos = null;
+    if(mouseDown > 0)
+    {
+        pos = getMousePos(canvas, e);
+    }
+    
+    
     for(i = 0; i < numPoints; i++)
     {
         var direction = Math.random();
@@ -58,17 +92,16 @@ function Update()
         else if(direction < 0.8)
             pointsVelocities[i][1] -= speed;
         
-        if(pointsVelocities[i][0] >= maxVelocity)pointsVelocities[i][0] = maxVelocity;
-        if(pointsVelocities[i][0] <= -maxVelocity)pointsVelocities[i][0] = -maxVelocity;
-        if(pointsVelocities[i][1] >= maxVelocity)pointsVelocities[i][1] = maxVelocity;
-        if(pointsVelocities[i][1] <= -maxVelocity)pointsVelocities[i][1] = -maxVelocity;
-    }
-    
-    for(i = 0; i < numPoints; i++)
-    {
-        pointsArray[i][0] += pointsVelocities[i][0];
-        pointsArray[i][1] += pointsVelocities[i][1];
+        //Mouse Attract
+        if(pos)
+        {
+            if(pointsArray[i][0] >= pos.x)pointsVelocities[i][0] -= speed/clickMoveInvPower;
+            if(pointsArray[i][0] <= pos.x)pointsVelocities[i][0] += speed/clickMoveInvPower;
+            if(pointsArray[i][1] >= pos.y)pointsVelocities[i][1] -= speed/clickMoveInvPower;
+            if(pointsArray[i][1] <= pos.y)pointsVelocities[i][1] += speed/clickMoveInvPower;
+        }
         
+        //Wall Bounce
         if(pointsArray[i][0] >= canvas.width)
         {
             pointsArray[i][0] = canvas.width;
@@ -89,37 +122,68 @@ function Update()
             pointsArray[i][1] = 0;
             pointsVelocities[i][1] += 1.0;
         }
+        
+        //Cap Max Velocity
+        if(pointsVelocities[i][0] >= maxVelocity)pointsVelocities[i][0] = maxVelocity;
+        if(pointsVelocities[i][0] <= -maxVelocity)pointsVelocities[i][0] = -maxVelocity;
+        if(pointsVelocities[i][1] >= maxVelocity)pointsVelocities[i][1] = maxVelocity;
+        if(pointsVelocities[i][1] <= -maxVelocity)pointsVelocities[i][1] = -maxVelocity;
+    }
+    
+    for(i = 0; i < numPoints; i++)
+    {
+        pointsArray[i][0] += pointsVelocities[i][0];
+        pointsArray[i][1] += pointsVelocities[i][1];
     }
     
     ///////////////////////////////////////////////////////////////
     //////////////////////////DRAW
     ///////////////////////////////////////////////////////////////
     ctx.clearRect (0, 0, ctx.canvas.width, ctx.canvas.height);
-    
+
     for(i = 0; i < numPoints; i++)
     {
         drawPoint(ctx, pointsArray[i], 3);
         
-        for(var j = 0; j < numPoints; j++)
+        for(var j = i; j < numPoints; j++)
         {
-            if(distanceCheck(pointsArray[i], pointsArray[j], distanceForLine))
+            var distanceBetweenPoints = distanceCheck(pointsArray[i], pointsArray[j]);
+            var distanceForLineSq = distanceForLine * distanceForLine;
+            if(distanceBetweenPoints < distanceForLineSq)
             {
-                drawLineBetweenPoints(ctx, pointsArray[i], pointsArray[j]);
+                var a1 = 1-(distanceBetweenPoints/distanceForLineSq);
+                var opacity = a1*a1 * 2;
+                
+                drawLineBetweenPoints(ctx, pointsArray[i], pointsArray[j], opacity);
             }
         }
-        
-        //drawPoint(ctx, point2, 5);
-        //
-        
     }
 }
 
 function Resize()
 {
     var canvas = document.getElementById('background');
+    var dpr = window.devicePixelRatio || 1;
+    var rect = canvas.getBoundingClientRect();
+    
+    var oldWidth = canvas.width;
+    var oldHeight = canvas.height;
+    
+    var newWidth = rect.width * dpr;
+    var newHeight = rect.height * dpr;
+    
+    if(Math.abs(newWidth - oldWidth) > resizeRespawnThreshold ||
+       Math.abs(newHeight - oldHeight) > resizeRespawnThreshold)
+    {
+        created = false;
+    }
+    
+    canvas.width = rect.width * dpr;
+    canvas.height = rect.height * dpr;
+    
+    var ctx = canvas.getContext('2d');
 
-    canvas.width = window.innerHeight;
-    canvas.height =  window.innerHeight;
+    ctx.scale(dpr, dpr);
     
     Update();
 }
@@ -127,13 +191,10 @@ function Resize()
 ///////////////////////////////////////////////////////////////
 //////////////////////////UTILITY
 ///////////////////////////////////////////////////////////////
-function distanceCheck(pointOne, pointTwo, r) {
+function distanceCheck(pointOne, pointTwo) {
     var dist_points = (pointOne[0] - pointTwo[0]) * (pointOne[0] - pointTwo[0]) + (pointOne[1] - pointTwo[1]) * (pointOne[1] - pointTwo[1]);
-    r *= r;
-    if (dist_points < r) {
-        return true;
-    }
-    return false;
+
+    return dist_points;
 }    
     
 function drawPoint(ctx, point, size)
@@ -144,12 +205,38 @@ function drawPoint(ctx, point, size)
     ctx.fill();
 }
 
-function drawLineBetweenPoints(ctx, pointOne, pointTwo)
+function drawLineBetweenPoints(ctx, pointOne, pointTwo, opacity)
 {
+    ctx.globalAlpha = opacity;
+    
     ctx.beginPath();
     ctx.moveTo(pointOne[0], pointOne[1]);
     ctx.lineTo(pointTwo[0], pointTwo[1]);
     ctx.strokeStyle = niceColour;
     ctx.lineWidth = 1;
     ctx.stroke()
+    
+    ctx.globalAlpha = 1;
+}
+
+function setMousePos(evt)
+{
+    if(evt)
+    {
+        lastMousePos = [evt.clientX, evt.clientY];
+    }
+}
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    
+    if(evt)
+    {
+        lastMousePos = [evt.clientX, evt.clientY];
+    }
+    
+    return {
+        x: (lastMousePos[0] - rect.left) / (rect.right - rect.left) * canvas.width,
+        y: (lastMousePos[1] - rect.top) / (rect.bottom - rect.top) * canvas.height
+    };
 }
